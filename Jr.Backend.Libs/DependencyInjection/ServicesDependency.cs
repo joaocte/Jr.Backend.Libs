88a431cbe485f13507d8b133e9529f5014691aaa
João Roberto de Oliveira Rêgo <joaocte@gmail.com>
2021-07-02 07:07:13 -0300

    Adicionado registro de dependencia.

---
+++ Jr.Backend.Libs/DependencyInjection/ServicesDependency.cs
@@ -0,0 +1,33 @@
+﻿using Jr.Backend.Libs.Infrastructure.Repository.MongoDb.Context;
+using Jr.Backend.Libs.Infrastructure.Repository.MongoDb.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jr.Backend.Libs.Infrastructure.DependencyInjection
+{
+    public static class ServicesDependency
+    {
+        public static void AddServiceDependency(this IServiceCollection services)
+        {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")?.ToLowerInvariant();
+            services.AddScoped<IMongoContext>((p) =>
+            {
+                var config = new ConfigurationBuilder()
+                                 .SetBasePath(Directory.GetCurrentDirectory())
+                                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: false)
+                                 .AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: false)
+                                 .AddEnvironmentVariables()
+                                 .Build();
+
+                return new MongoContext(config);
+            });
+        }
+    }
+}

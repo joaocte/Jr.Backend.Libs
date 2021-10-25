52c5cdfe7fe8c69b6979c2f3270caf8f85b7443b
João Roberto de Oliveira Rêgo <joaocte@gmail.com>
2021-10-25 20:48:07 -0300

    Correções no build do projeto de teste.

--- Jr.Backend.Libs.Tests/TestObjjects/Messaging/MessageFake.cs
+++ Jr.Backend.Libs.Tests/TestObjjects/Messaging/MessageFake.cs
@@ -1,4 +1,6 @@
-﻿namespace Jr.Backend.Libs.Tests.TestObjjects.Messaging
+﻿using Jr.Backend.Libs.Messaging.Abstractions.Interfaces;
+
+namespace Jr.Backend.Libs.Tests.TestObjjects.Messaging
 {
     public class MessageFake : IEvent
     {
--- Jr.Backend.Libs.Tests/TestObjjects/Messaging/MessageFakeFormatter.cs
+++ Jr.Backend.Libs.Tests/TestObjjects/Messaging/MessageFakeFormatter.cs
@@ -1,4 +1,6 @@
-﻿namespace Jr.Backend.Libs.Tests.TestObjjects.Messaging
+﻿using Jr.Backend.Libs.Messaging.Abstractions;
+
+namespace Jr.Backend.Libs.Tests.TestObjjects.Messaging
 {
     public class MessageFakeFormatter : MessageFormatterBase<MessageFake>
     {
